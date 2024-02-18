5009430800b8975808a5bb7d0784154b990a8d1f
Griffin Ferguson <griffinferguson.us@gmail.com>
2024-02-18 01:18:18 -0600

    add let's code logo and fonts

--- export.sh
+++ export.sh
@@ -4,5 +4,4 @@
 
 # SVG to PNG
 #seal.svg
-inkscape "$(pwd)/seal.svg" --export-filename "$(pwd)/seal.png" --export-width=1500 --export-height=1500 --export-background-opacity=0.0
-inkscape "$(pwd)/logo.svg" --export-filename "$(pwd)/logo.png" --export-width=1500 --export-height=1500 --export-background-opacity=0.0
+inkscape "$1.svg" --export-filename "$1.png" --export-width=1500 --export-height=1500 --export-background-opacity=0.0
